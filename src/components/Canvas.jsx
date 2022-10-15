033dd546b5223177304141e552f2633befbda48c
atlas <a.sproson@outlook.com>
2022-10-15 14:15:16 +0100

    Creates reusable platforms

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -60,10 +60,10 @@ const Canvas = () => {
   }
 
   class Platform {
-    constructor() {
+    constructor({ x, y }) {
       this.position = {
-        x: 200,
-        y: 100,
+        x: x,
+        y: y,
       };
       this.width = 200;
       this.height = 20;
@@ -80,7 +80,10 @@ const Canvas = () => {
 
   const player = new Player();
 
-  const platform = new Platform();
+  const platforms = [
+    new Platform({ x: 200, y: 100 }),
+    new Platform({ x: 500, y: 300 }),
+  ];
 
   const keys = {
     right: {
@@ -99,7 +102,9 @@ const Canvas = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       player.update();
-      platform.draw();
+      platforms.forEach((platform) => {
+        platform.draw();
+      });
 
       if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 5;
@@ -108,22 +113,28 @@ const Canvas = () => {
       } else {
         player.velocity.x = 0;
         if (keys.right.pressed) {
-          platform.position.x -= 5;
+          platforms.forEach((platform) => {
+            platform.position.x -= 5;
+          });
         } else if (keys.left.pressed) {
-          platform.position.x += 5;
+          platforms.forEach((platform) => {
+            platform.position.x += 5;
+          });
         }
       }
 
       // Platform collision
-      if (
-        player.position.y + player.height <= platform.position.y &&
-        player.position.y + player.height + player.velocity.y >=
-          platform.position.y &&
-        player.position.x + player.width >= platform.position.x &&
-        player.position.x <= platform.position.x + platform.width
-      ) {
-        player.velocity.y = 0;
-      }
+      platforms.forEach((platform) => {
+        if (
+          player.position.y + player.height <= platform.position.y &&
+          player.position.y + player.height + player.velocity.y >=
+            platform.position.y &&
+          player.position.x + player.width >= platform.position.x &&
+          player.position.x <= platform.position.x + platform.width
+        ) {
+          player.velocity.y = 0;
+        }
+      });
     }
   };
 
